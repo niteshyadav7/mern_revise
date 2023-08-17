3d2635bd1c69008125ce82a02430dfb4008e3110
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-08-18 03:18:12 +0530

    js

---
+++ JavaScript/index.js
@@ -0,0 +1,62 @@
+// getName();
+// console.log(x);
+// var x = 4;
+// console.log(x);
+// function getName() {
+//   console.log("Namaste JavaScript!");
+// }
+
+// var a = 2;
+// b();
+// function b() {
+//  var a = 23;
+//   console.log("Namaste JavaScript!");
+//   console.log(this.a);
+// }
+// console.log(a);
+
+// console.log(x);
+// var x = 3;
+// console.log(x); //3
+// console.log(a); //a is not defined =>Reference Error.
+
+//   var a = 23;
+//   let b = 54;
+//   const c = 63;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// // var a = 5;
+// let a=43;
+// {
+// //   var a = 32;
+// let a=54
+//   console.log(a);
+// }
+// console.log(a);
+
+// function x() {
+//   for (var i = 1; i <= 5; i++) {
+//     console.log(i);
+//     setTimeout(() => {
+//     //   console.log(i);
+//     console.log("Namaste");
+//     }, i * 1000);
+//   }
+// }
+// x();
+
+// function x() {
+//   let a = 32;
+//   let b = 23;
+//   function y( b) {
+//     // console.log();
+//     // console.log();
+//     return a + b;
+//   }
+//   return y;
+// }
+// const res = x();
+// console.log(console.log(res(43)));
